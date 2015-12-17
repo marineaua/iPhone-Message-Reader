adecabfc5a3a194f7642dc1950ee4ab636c690fa
Alex Marineau <alexmarineau@yahoo.com>
2015-12-16 22:29:12 -0800

    Initial Setup, SQLite Connection working

---
+++ iPhoneBackupMessageReader/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace iPhoneBackupMessageReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDatabaseFileImport = new System.Windows.Forms.Button();
+            this.btnConnectToDatabase = new System.Windows.Forms.Button();
+            this.btnCloseConnection = new System.Windows.Forms.Button();
+            this.textAreaMessages = new System.Windows.Forms.RichTextBox();
+            this.btnShowMessages = new System.Windows.Forms.Button();
+            this.btnFillContacts = new System.Windows.Forms.Button();
+            this.listBoxContacts = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnDatabaseFileImport
+            // 
+            this.btnDatabaseFileImport.Location = new System.Drawing.Point(12, 347);
+            this.btnDatabaseFileImport.Name = "btnDatabaseFileImport";
+            this.btnDatabaseFileImport.Size = new System.Drawing.Size(75, 23);
+            this.btnDatabaseFileImport.TabIndex = 0;
+            this.btnDatabaseFileImport.Text = "Import";
+            this.btnDatabaseFileImport.UseVisualStyleBackColor = true;
+            this.btnDatabaseFileImport.Click += new System.EventHandler(this.importData_Click);
+            // 
+            // btnConnectToDatabase
+            // 
+            this.btnConnectToDatabase.Location = new System.Drawing.Point(93, 347);
+            this.btnConnectToDatabase.Name = "btnConnectToDatabase";
+            this.btnConnectToDatabase.Size = new System.Drawing.Size(75, 23);
+            this.btnConnectToDatabase.TabIndex = 1;
+            this.btnConnectToDatabase.Text = "Connect";
+            this.btnConnectToDatabase.UseVisualStyleBackColor = true;
+            this.btnConnectToDatabase.Click += new System.EventHandler(this.btnConnectToDatabase_Click);
+            // 
+            // btnCloseConnection
+            // 
+            this.btnCloseConnection.Location = new System.Drawing.Point(174, 347);
+            this.btnCloseConnection.Name = "btnCloseConnection";
+            this.btnCloseConnection.Size = new System.Drawing.Size(75, 23);
+            this.btnCloseConnection.TabIndex = 2;
+            this.btnCloseConnection.Text = "Close";
+            this.btnCloseConnection.UseVisualStyleBackColor = true;
+            this.btnCloseConnection.Click += new System.EventHandler(this.btnCloseConnection_Click);
+            // 
+            // textAreaMessages
+            // 
+            this.textAreaMessages.Location = new System.Drawing.Point(236, 12);
+            this.textAreaMessages.Name = "textAreaMessages";
+            this.textAreaMessages.Size = new System.Drawing.Size(527, 281);
+            this.textAreaMessages.TabIndex = 3;
+            this.textAreaMessages.Text = "";
+            // 
+            // btnShowMessages
+            // 
+            this.btnShowMessages.Location = new System.Drawing.Point(688, 347);
+            this.btnShowMessages.Name = "btnShowMessages";
+            this.btnShowMessages.Size = new System.Drawing.Size(75, 23);
+            this.btnShowMessages.TabIndex = 4;
+            this.btnShowMessages.Text = "Show";
+            this.btnShowMessages.UseVisualStyleBackColor = true;
+            this.btnShowMessages.Click += new System.EventHandler(this.btnShowMessages_Click);
+            // 
+            // btnFillContacts
+            // 
+            this.btnFillContacts.Location = new System.Drawing.Point(255, 347);
+            this.btnFillContacts.Name = "btnFillContacts";
+            this.btnFillContacts.Size = new System.Drawing.Size(75, 23);
+            this.btnFillContacts.TabIndex = 5;
+            this.btnFillContacts.Text = "Fill";
+            this.btnFillContacts.UseVisualStyleBackColor = true;
+            this.btnFillContacts.Click += new System.EventHandler(this.btnFillContacts_Click);
+            // 
+            // listBoxContacts
+            // 
+            this.listBoxContacts.FormattingEnabled = true;
+            this.listBoxContacts.Location = new System.Drawing.Point(12, 12);
+            this.listBoxContacts.Name = "listBoxContacts";
+            this.listBoxContacts.Size = new System.Drawing.Size(218, 277);
+            this.listBoxContacts.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 382);
+            this.Controls.Add(this.listBoxContacts);
+            this.Controls.Add(this.btnFillContacts);
+            this.Controls.Add(this.btnShowMessages);
+            this.Controls.Add(this.textAreaMessages);
+            this.Controls.Add(this.btnCloseConnection);
+            this.Controls.Add(this.btnConnectToDatabase);
+            this.Controls.Add(this.btnDatabaseFileImport);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDatabaseFileImport;
+        private System.Windows.Forms.Button btnConnectToDatabase;
+        private System.Windows.Forms.Button btnCloseConnection;
+        private System.Windows.Forms.RichTextBox textAreaMessages;
+        private System.Windows.Forms.Button btnShowMessages;
+        private System.Windows.Forms.Button btnFillContacts;
+        private System.Windows.Forms.ListBox listBoxContacts;
+    }
+}
+
---
+++ iPhoneBackupMessageReader/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace iPhoneBackupMessageReader
+{
+    public partial class Form1 : Form
+    {
+        string messageConnectionString;
+        string contactConnectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //31bb7ba8914766d4ba40d6dfb6113c8b614be442 for address
+            contactConnectionString = @" Data Source = C:\Users\alexm_000\Desktop\iOS_BACKUP\31bb7ba8914766d4ba40d6dfb6113c8b614be442; Version = 3 ";
+            messageConnectionString = @" Data Source = C:\Users\alexm_000\Desktop\iOS_BACKUP\3d0d7e5fb2ce288813306e4d4636395e047a3d28; Version = 3 ";
+        }
+
+        private void importData_Click(object sender, EventArgs e)
+        {
+            //// Create an instance of the open file dialog box.
+            //OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            //// Set filter options and filter index.
+            //openFileDialog1.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            //openFileDialog1.FilterIndex = 1;
+
+            //// Process input if the user clicked OK.
+            //if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            //{
+            //    // Open the selected file to read.
+            //    System.IO.Stream fileStream = openFileDialog1.OpenFile();
+
+            //    using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
+            //    {
+            //        // Read the first line from the file and write it the textbox.
+            //        richTextBox1.Text = reader.ReadLine();
+            //    }
+            //    fileStream.Close();
+            //}
+        }
+
+        private void btnConnectToDatabase_Click(object sender, EventArgs e)
+        {
+            using (SQLiteConnection con = new SQLiteConnection(messageConnectionString))
+            {
+                try
+                {
+                    con.Open();
+                    if (con.State == ConnectionState.Open)
+                    {
+                        MessageBox.Show("Connection created successfully...");
+                    }
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+            
+        }
+
+        private void btnCloseConnection_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection con = new SQLiteConnection(messageConnectionString);
+            con.Close();
+        }
+
+        private void btnShowMessages_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection con = new SQLiteConnection(messageConnectionString);
+            con.Open();
+            string sql = "SELECT * FROM message WHERE handle_id = 1";
+            SQLiteCommand command = new SQLiteCommand(sql, con);
+            SQLiteDataReader reader = command.ExecuteReader();
+            //int i = 1;
+            while (reader.Read())
+            {
+                textAreaMessages.Text += (reader["text"].ToString() + "\n");
+                //Console.WriteLine(i + " : " +  reader["text"].ToString());
+                //i++;
+            }
+            //Console.ReadLine();
+            
+            reader.Close();
+            con.Close();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFillContacts_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection con = new SQLiteConnection(contactConnectionString);
+            con.Open();
+            string sql = "SELECT * FROM ABPerson ORDER BY First";
+            SQLiteCommand command = new SQLiteCommand(sql, con);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                //ListViewItem newPerson;
+                //string[] personArray = new string[2];
+                //personArray[0] = reader["First"].ToString();
+                //personArray[1] = reader["Last"].ToString();
+                //newPerson = new ListViewItem(personArray);
+                //listViewContacts.Items.Add(newPerson);
+                listBoxContacts.Items.Add(reader["First"].ToString() + " " + reader["Last"].ToString());
+            }
+
+            reader.Close();
+            con.Close();
+        }
+    }
+}
+
